1c8ef461772c67a9b19a9694112d569de5696218
Köksal Öcal <121344310+stockholmsw@users.noreply.github.com>
2023-08-06 17:42:16 +0300

    Update Homework3.sql

--- Homework3.sql
+++ Homework3.sql
@@ -1,8 +1,11 @@
 SELECT country FROM country
 WHERE country LIKE 'A%a';
+
 SELECT country FROM country
 WHERE country LIKE '%_____n';
+
 SELECT title FROM film
 WHERE title ILIKE '%T%T%T%T';
+
 SELECT title,rental_rate,length FROM film
 WHERE title LIKE 'C%' AND length >90 AND rental_rate = 2.99;

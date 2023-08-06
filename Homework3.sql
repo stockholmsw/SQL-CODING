c5cfedc2d2e1b9b255ebc06c8e566eb6831bbce8
Köksal Öcal <121344310+stockholmsw@users.noreply.github.com>
2023-08-06 17:44:07 +0300

    Update Homework3.sql

--- Homework3.sql
+++ Homework3.sql
@@ -1,7 +1,7 @@
 --country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
 SELECT country FROM country
 WHERE country LIKE 'A%a';
---ountry tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
+--country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
 SELECT country FROM country
 WHERE country LIKE '%_____n';
 --film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.

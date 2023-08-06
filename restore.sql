c8383d5d4eaec88b9596667f8cd7a40d5410f4b8
= <koksalocal06@gmail.com>
2023-08-06 17:08:20 +0300

    ilk ödev yapıldı.

--- restore.sql
+++ restore.sql
@@ -4,4 +4,3 @@ SELECT rental_rate,replacement_cost  FROM film
 WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99);
 SELECT * FROM customer WHERE first_name = 'Mary';
 SELECT * FROM film WHERE  NOT length >50 AND  NOT (rental_rate = 2.99 OR rental_rate = 4.99); 
-											

f03d9f32130b237cf19cc5965ad477740f47f2b9
= <koksalocal06@gmail.com>
2023-08-06 17:23:47 +0300

    ikinci ödev yapıldı.

--- restore.sql
+++ restore.sql
@@ -1,6 +1,6 @@
-SELECT title,description FROM film;
-SELECT * FROM film WHERE length >60 OR LENGTH<75;
-SELECT rental_rate,replacement_cost  FROM film
-WHERE rental_rate = 0.99 AND (replacement_cost = 12.99 OR replacement_cost = 28.99);
-SELECT * FROM customer WHERE first_name = 'Mary';
-SELECT * FROM film WHERE  NOT length >50 AND  NOT (rental_rate = 2.99 OR rental_rate = 4.99); 
+SELECT replacement_cost FROM film
+WHERE replacement_cost BETWEEN 12.99 AND 16.99;
+SELECT first_name,last_name FROM actor
+WHERE first_name IN('Penelope','Nick','Ed');
+SELECT rental_rate,replacement_cost FROM film
+WHERE rental_rate  IN (0.99,2.99,4.99)  AND replacement_cost IN ( 12.99,15.99,28.99);
